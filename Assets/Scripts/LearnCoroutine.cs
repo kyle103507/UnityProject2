3065144688f46b50e79ff6b4473c24a6d4e81ed0
kyle103507 <61506510+kyle103507@users.noreply.github.com>
2020-08-16 20:37:27 +0800

    Initial commit

---
+++ Assets/Scripts/LearnCoroutine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+
+public class LearnCoroutine : MonoBehaviour
+{
+    public Transform ming;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Big());
+    }
+
+    void Update()
+    {
+        ming.RotateAround(Vector3.zero, Vector3.one*5, 20f );
+        
+
+    }
+
+
+    public IEnumerator Big()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            ming.localScale += Vector3.one;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/LearnLoop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LearnLoop : MonoBehaviour
+{
+
+    public Transform cude;
+    private int i = 1;
+    private void Start()
+    {
+        while (i < 6)
+        {
+            print("我是迴圈" +i);
+            i++;
+        }
+
+        for (int i = 1; i < 6; i++)
+        {
+            print("我是迴圈FOR" +i);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 pos = new Vector3(i, 0, 0);
+            Instantiate(cude, pos, Quaternion.identity);
+        }
+    }
+}
